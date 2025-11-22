b6fd6316ba7394f90f3cad7f6a3fe6c1186f65e0
Mathis Le Bonniec <mathislbonniec@gmail.com>
2025-11-23 00:20:24 +0100

    feat(manager): create the QuickActionsManager

--- Sources/QuickActionsKit/QuickActionsManager.swift
+++ Sources/QuickActionsKit/QuickActionsManager.swift
@@ -8,8 +8,24 @@
 
 import UIKit
 
-final class QuickActionsManager {
+final public class QuickActionsManager<T> where T: QuickActions {
     // MARK: Properties
+    private var configuration: T
+
     // MARK: Lifecycle
+    public init(_ configuration: T) {
+        self.configuration = configuration
+    }
+
     // MARK: Methods
+    @MainActor
+    public func update() {
+        // TODO: Refresh QuickActions
+        let application = UIApplication.shared
+        let actions = configuration.getDynamicActions().union(configuration.getDefaultActions())
+        let available = actions.compactMap(QuickActionsMapper.map)
+        let empty = configuration.getEmptyActions().compactMap(QuickActionsMapper.map)
+
+        application.shortcutItems = available.isEmpty ? empty : available
+    }
 }
