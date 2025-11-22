a137359ddb11d49706ed9a7a4d9b3ae8b1bb28d3
Mathis Le Bonniec <mathislbonniec@gmail.com>
2025-11-23 00:19:32 +0100

    feat(QuickActions): create the QuickActions protocol

--- Sources/QuickActionsKit/QuickActions.swift
+++ Sources/QuickActionsKit/QuickActions.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+public protocol QuickActions {
+    associatedtype T: QuickActionType
+
+    /// The list of dynamic actions.
+    func getDynamicActions() -> Set<QuickActionsItem<T>>
+
+    /// The list of actions required.
+    func getDefaultActions() -> Set<QuickActionsItem<T>>
+
+    /// The list of actions to show when the dynamic actions are all hidden.
+    func getEmptyActions() -> Set<QuickActionsItem<T>>
+}
